c6e7960d05c2cc155c10bfeda30e9f3390dc02d1
María Gabaldón Rico <mariag@stampery.co>
2020-09-04 16:55:28 +0200

    feat: create responsive

--- pages/index.vue
+++ pages/index.vue
@@ -20,13 +20,18 @@
       </p>
       <div class="links">
         <a
+          class="link"
           href="https://github.com/witnet/sheikah/releases/download/0.2.0/sheikah-witnet-wallet-0.2.0.AppImage"
           download="Sheikah GNU/Linux"
         >
-          <ElButton type="primary">Download Sheikah for GNU/Linux </ElButton>
+          <ElButton class="btn" type="primary">
+            Download Sheikah for GNU/Linux
+          </ElButton>
         </a>
         <a class="link" href="" download="Sheikah macOS">
-          <ElButton type="primary">Download Sheikah for macOS</ElButton>
+          <ElButton class="btn" type="primary">
+            Download Sheikah for macOS
+          </ElButton>
         </a>
       </div>
       <p class="footer">
@@ -65,7 +70,7 @@ export default {}
     align-items: center;
     .logo-title {
       margin-left: 16px;
-      font-size: 36px;
+      font-size: 2em;
       color: $alt-grey;
       font-weight: bold;
       font-family: 'Catamaran';
@@ -78,8 +83,9 @@ export default {}
     color: $black;
     font-family: 'Raleway';
     text-align: left;
-    font-size: 48px;
+    font-size: 3em;
     font-weight: bolder;
+    max-width: 500px;
     margin-top: 32px;
   }
   .description {
@@ -87,19 +93,87 @@ export default {}
     font-weight: 400;
     margin-top: 16px;
     text-align: left;
-    font-size: 22px;
+    font-size: 1.3em;
   }
   .links {
     display: flex;
     margin-top: 24px;
     .link {
-      margin-left: 8px;
+      &:last-of-type {
+        margin-left: 8px;
+      }
     }
   }
   .footer {
     color: $grey;
     text-align: left;
-    margin-top: 24px;
+    margin-top: 16px;
+  }
+}
+
+@media (max-width: 1200px) and (min-width: 768px) {
+  .container {
+    flex-direction: column;
+    justify-content: space-around;
+    .app-image {
+      display: block;
+      margin-left: 0;
+    }
+    .left-column {
+      max-width: 90vw;
+      .header {
+        margin-top: 16px;
+      }
+      .footer {
+        margin-bottom: 16px;
+      }
+      .links {
+        width: 100%;
+        flex-direction: row;
+      }
+      .btn {
+        width: 100%;
+      }
+      .link {
+        width: 100%;
+        &:last-of-type {
+          margin-left: 8px;
+          justify-content: center;
+        }
+      }
+    }
+  }
+}
+
+@media (max-width: 700px) {
+  .app-image {
+    display: none;
+  }
+  .container {
+    .left-column {
+      max-width: 90vw;
+      min-height: 100vh;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      .header {
+        margin-top: 16px;
+      }
+      .footer {
+        margin-bottom: 16px;
+      }
+      .links {
+        flex-direction: column;
+      }
+      .btn {
+        width: 100%;
+      }
+      .link:last-of-type {
+        margin-top: 8px;
+        margin-left: 0;
+        justify-content: center;
+      }
+    }
   }
 }
 </style>
