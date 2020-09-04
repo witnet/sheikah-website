c77c888f51f0e0a6576b977ec4cf4dc05b0999d0
María Gabaldón Rico <mariag@stampery.co>
2020-09-04 10:44:13 +0200

    feat: add custom icon

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <div>
       <Logo />
-      <h1 class="title">y</h1>
+      <h1 class="title">Sheikah</h1>
       <div class="links">
         <a
           href="https://nuxtjs.org/"
