d79767cdfd2d968a9c33809d1de6f2776a62ad44
tommytrg <tomastrg22@gmail.com>
2020-10-13 18:24:10 +0200

    chore: update releases url

--- constants.js
+++ constants.js
@@ -2,9 +2,9 @@ export const DEFAULT_OS = 'Linux'
 
 export const RELEASE = {
   LINUX:
-    'https://github.com/witnet/sheikah/releases/download/v1.0.1/Sheikah-Witnet-wallet-1.0.1.AppImage',
+    'https://github.com/witnet/sheikah/releases/download/v1.1.0/Sheikah-Witnet-wallet-1.1.0.AppImage',
   WIN:
-    'https://github.com/witnet/sheikah/releases/download/v1.0.1/Sheikah.Witnet.wallet.Setup.1.0.1.exe',
+    'https://github.com/witnet/sheikah/releases/download/v1.1.0/Sheikah-Witnet-wallet-Setup-1.1.0.exe',
   MAC:
-    'https://github.com/witnet/sheikah/releases/download/v1.0.1/Sheikah.Witnet.wallet-1.0.1.dmg',
+    'https://github.com/witnet/sheikah/releases/download/v1.1.0/Sheikah-Witnet-wallet-1.1.0.dmg',
 }
