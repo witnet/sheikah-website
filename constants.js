2bf887d7b85c76009b7508e570951c781500959d
tommytrg <tomastrg22@gmail.com>
2020-10-14 16:07:11 +0200

    chore: upgrade download links

--- constants.js
+++ constants.js
@@ -2,9 +2,9 @@ export const DEFAULT_OS = 'Linux'
 
 export const RELEASE = {
   LINUX:
-    'https://github.com/witnet/sheikah/releases/download/v1.1.0/Sheikah-Witnet-wallet-1.1.0.AppImage',
+    'https://github.com/witnet/sheikah/releases/download/v1.1.1/Sheikah-Witnet-wallet-1.1.1.AppImage',
   WIN:
-    'https://github.com/witnet/sheikah/releases/download/v1.1.0/Sheikah-Witnet-wallet-Setup-1.1.0.exe',
+    'https://github.com/witnet/sheikah/releases/download/v1.1.1/Sheikah-Witnet-wallet-Setup-1.1.1.exe',
   MAC:
-    'https://github.com/witnet/sheikah/releases/download/v1.1.0/Sheikah-Witnet-wallet-1.1.0.dmg',
+    'https://github.com/witnet/sheikah/releases/download/v1.1.1/Sheikah-Witnet-wallet-1.1.1.dmg',
 }
