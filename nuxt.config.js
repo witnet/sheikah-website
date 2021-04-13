5161a1608c7caa49cb87f41bdd62c6ccffc6e206
tommytrg <tomastrg22@gmail.com>
2021-04-13 16:19:00 +0200

    fix: load fonts as static nuxt assets

--- nuxt.config.js
+++ nuxt.config.js
@@ -41,7 +41,29 @@ export default {
       { name: 'og:url', content: 'https://sheikah.app' },
       { name: 'og:locale:alternate', content: 'es_ES' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/sheikah.ico' }],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/sheikah.ico' },
+      {
+        rel: 'stylesheet',
+        href:
+          'https://fonts.googleapis.com/css2?family=Catamaran:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap',
+      },
+      {
+        rel: 'stylesheet',
+        href:
+          'https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap',
+      },
+      {
+        rel: 'stylesheet',
+        href:
+          'https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100;0,300;0,400;0,500;0,700;1,100;1,300;1,400;1,500;1,700&display=swap',
+      },
+      {
+        rel: 'stylesheet',
+        href:
+          'https://fonts.googleapis.com/css2?family=Raleway:wght@800&display=swap',
+      },
+    ],
   },
   /*
    ** Global CSS
